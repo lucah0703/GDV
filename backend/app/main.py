753339a668cdadf5defbce7fb53fbd15179c934e
Lucia Canzian <3003493@stud.hs-mannheim.de>
2026-06-10 14:47:11 +0200

    add route for history num bike for specific station; add new csv-Data for history

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from backend.app.routes import routes
+from backend.app.routes import routes, routes_availability
 
 app = FastAPI()
 
--- backend/app/routes/routes_availability.py
+++ backend/app/routes/routes_availability.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, HTTPException
 import requests
 from datetime import datetime
+import pandas as pd
 
 router = APIRouter(tags=["Availability"])
 
-NEXTBIKE_MA_STATIONSTATUS_URL = "https://api.mobidata-bw.de/sharing/gbfs/v2/nextbike_nn/station_status"
+#TODO: URL für Karlsruhe und Stuttgart finden
+NEXTBIKE_MA_STATIONSTATUS_URL = "https://api.mobidata-bw.de/sharing/gbfs/v2/nextbike_vn/station_status"
+resource_ckan = "37b0e14c-8347-4155-a859-3754841a020f"
 
+#TODO: Ausgewählte Stadt vom Frontend bekommen
 @router.get("/availability/current/{station_id}")
 async def get_current_availability(station_id: str):
 
@@ -27,8 +31,28 @@ async def get_current_availability(station_id: str):
         "last_reported": datetime.fromtimestamp(station["last_reported"])
     } 
 
+#TODO: CSV-Datei Umfang festlegen
 @router.get("/availability/history/{station_id}")
-async def get_history_availability(station_id: int):
+async def get_history_availability(station_id: str):
     
+    df = pd.read_csv("backend/data/availability/wanted_kw21.csv")
 
-    return
+    df['last_reported_clean'] = df['last_reported'].str.slice(0, 19)
+    df['last_reported_clean'] = pd.to_datetime(df['last_reported_clean'])
+
+    df_filtered = df[df['station_id'] == station_id].copy()
+
+    if df_filtered.empty:
+        raise HTTPException(status_code=404, detail="Station not found")
+
+    df_filtered['stunde'] = df_filtered['last_reported_clean'].dt.hour
+    df_hourly = df_filtered.groupby('stunde')['num_bicycles_available'].mean().reset_index()
+    df_hourly['num_bicycles_available'] = df_hourly['num_bicycles_available'].round(2)
+
+    x_data = df_hourly['stunde'].tolist()
+    y_data = df_hourly['num_bicycles_available'].tolist()
+    
+    return {
+        "x_data":x_data,
+        "y_data": y_data
+    }
