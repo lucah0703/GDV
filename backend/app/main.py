966cb17a60c16eb71a81ad7d7b554a48df6e1a7d
Lucia Canzian <3003493@stud.hs-mannheim.de>
2026-06-13 18:29:22 +0200

    add routes for bike stations

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from backend.app.routes import routes, routes_availability
+from backend.app.routes import routes, routes_availability, routes_station
 
 app = FastAPI()
 
 app.include_router(routes.router)
 app.include_router(routes_availability.router)
+app.include_router(routes_station.router)
 
 @app.get("/")
 def root():
--- backend/app/routes/routes_station.py
+++ backend/app/routes/routes_station.py
@@ -2,31 +2,27 @@ from fastapi import APIRouter, HTTPException
 import requests
 from datetime import datetime
 import pandas as pd
+import os
 
 router = APIRouter(tags=["Stations"])
-
-#TODO: URL für Karlsruhe und Stuttgart finden
-NEXTBIKE_MA_STATIONSTATUS_URL = "https://api.mobidata-bw.de/sharing/gbfs/v2/nextbike_vn/station_status"
-
+DATA_DIR = "backend/data/stations/"
 
 #TODO: Ausgewählte Stadt vom Frontend bekommen
-@router.get("/availability/current/{station_id}")
-async def get_current_availability(station_id: str):
+@router.get("/station/{city}")
+async def get_station_information(city: str):
 
-    gbfs_stationstatus = requests.get(NEXTBIKE_MA_STATIONSTATUS_URL).json()
+    city = city.lower()
 
-    station = next(
-    (s for s in gbfs_stationstatus["data"]["stations"]
-     if s["station_id"] == station_id),
-    None
-    )
-
-    # Minimaler Debugging
-    if station is None:
-        raise HTTPException(status_code=404, detail="Station not found")
-
-    return {
-        "station_id": station_id,
-        "num_bikes_available": station["num_bikes_available"],
-        "last_reported": datetime.fromtimestamp(station["last_reported"])
-    }
+    if city == "stuttgart":
+        file_name = "STU_regiorad_stationinfo.csv"
+    elif city == "mannheim":
+        file_name = "MA_nextbike_stationinfo.csv"
+    elif city == "karlsruhe":
+        file_name = "KA_nextbike_stationinfo.csv"
+    else:
+        raise HTTPException(status_code=404, detail="City not found")
+    
+    file_path = os.path.join(DATA_DIR, file_name)
+    df = pd.read_csv(file_path, encoding="utf-8")
+    
+    return df.to_dict(orient="records")
