cc4b6b9ef69a2cfc0f28d055347c3aed4db7293c
fabiennehahn <fabienne_hahn@gmx.de>
2026-06-16 14:38:56 +0200

    backednd mit frontend verbunden

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.routes import routes_availability, routes_station #routen von Lucia
 from backend.app.routes import routes_isochrone, routes_pois, routes_pricing #routen von Luca
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(routes_availability.router)
 app.include_router(routes_station.router)
